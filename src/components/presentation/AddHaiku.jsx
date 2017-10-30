b90b43d6e39e811bf4b57fa7b4af6e9da867e23c
John Marinelli <john.marinelli.dev@gmail.com>
2017-10-30 16:59:03 -0700

    Add simple alert() based feedback

--- src/components/presentation/AddHaiku.jsx
+++ src/components/presentation/AddHaiku.jsx
@@ -17,12 +17,21 @@ let AddHaiku = ({
 }) => {
   const dispatchAddHaiku = (haiku) => {
     const expectedCounts = [5,3,5];
+    let ret = null;
 
     if (validateSyllables(haiku.lines, expectedCounts)) {
-      dispatch(addHaiku(haiku)).then(() => 
-        clearInputs()
-      );
+      dispatch(addHaiku(haiku))
+        .then(() => {
+          alert('Haiku posted.  Check out the Browse page to see it!');
+          ret = clearInputs();
+        })
+        .catch(reason => alert(`Haiku failed to post: ${reason}`));
     }
+    else {
+      alert('Haiku fails the 5-3-5 syllablic structure.');
+    }
+
+    return ret;
   };
 
   return (
