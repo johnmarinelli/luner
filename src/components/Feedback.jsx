29941030430e00af504e463afe3f50e75f3f9ae5
John Marinelli <john.marinelli.dev@gmail.com>
2017-11-06 12:57:24 -0800

    Static feedback for now

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -16,29 +16,8 @@ const Feedback = ({
   let feedback = null;
 
   if ('/' === pathname) {
-    const { lines } = haiku;
-    const expectedCount = [5,3,5];
-
-    let text = 'You are not done yet';
-
-    if (validateSyllables(lines, expectedCount)) {
-      text = 'You are done!';
-    }
-
-    else {
-      const syllables = lines.map(line => line.syllables);
-      const firstState = { counts: [0, 0, 0], text: 'A Lune is a 5-3-5 haiku.' },
-        secondState = { counts: [5, 0, 0], text: 'Good job!  You\'re almost there.' },
-        thirdState = { counts: [5, 3, 0], text: 'One more line to go.' };
-
-      text = [firstState, secondState, thirdState].reduce((acc, state) => {
-        if (compareArrays(state.counts, syllables)) return state.text;  
-        else return acc;
-      }, firstState.text);
-    }
-
     feedback = <div className='feedback'>
-      {text} 
+      The American Haiku has a 5-3-5 syllabic structure.  Go ahead, write your own below!
     </div>
   }
 
