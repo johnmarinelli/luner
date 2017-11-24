f443bccd1b77200e86204c801970b1cb9856039a
John Marinelli <john.marinelli.dev@gmail.com>
2017-11-24 15:13:42 -0800

    Fix focus bug

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
-import InputView from './Input';
+import { omit } from 'lodash';
+
+import './styles.css';
 
 class Input extends React.Component {
-  render () {
-    return <InputView {...this.props} />
+
+  focus () {
+    return this.htmlElement.focus();
   }
-};
+
+  getValue () {
+    return this.htmlElement.value;
+  }
+
+  setValue (val) {
+    this.htmlElement.value = val;
+    return val;
+  }
+
+  constructor () {
+    super();
+    this.htmlElement = null;
+    this.focus = this.focus.bind(this);
+    this.getValue = this.getValue.bind(this);
+    this.setValue = this.setValue.bind(this);
+  }
+
+  render () {
+    const { props } = this;
+    const { children } = props;
+    const newProps = omit(props, 'children');
+
+    return (
+      <input 
+        ref={el => this.htmlElement = el} 
+        {...newProps}>
+        {children}
+      </input>
+    );
+  }
+}
 
 export default Input;
